07bd3d14f2a6ca466c006dbd8916560d57325813
Eniola <olowoyoeniola@gmail.com>
2019-09-27 01:56:19 +0100

    work on 'at a glance' section of the dashboard

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, Route, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements  {
+
+export class AuthGuard implements CanActivate {
   
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    throw new Error("Method not implemented.");
+  }
+
+  constructor(private _router: Router) {
+    
+  }
+
 }
