fe062b8c7875d51af64b614e610e1211ced4be0b
Smahrt <msmahrt@gmail.com>
2019-10-05 06:57:27 +0100

    layout fixes + routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,8 @@ import { RegisterComponent } from './register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { TeamComponent } from './team/team.component';
+import { AboutComponent } from './about/about.component';
 
 
 const routes: Routes = [
@@ -15,6 +17,8 @@ const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'reset-password/:token', component: ResetPasswordComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'team', component: TeamComponent },
+  { path: 'about', component: AboutComponent },
   { path: 'dashboard', component: DashboardComponent },
 ];
 
